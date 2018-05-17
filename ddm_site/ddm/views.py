dfd8c1f81c9d6d7350d344049473ff7159ffc534
sirrah23 <hseth93@gmail.com>
2018-05-17 00:06:20 -0400

    Created a basic project view

--- ddm_site/ddm/views.py
+++ ddm_site/ddm/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Project
+
 
 def index(request):
+    project_list = Project.objects.order_by()
     template = loader.get_template('ddm/index.html')
-    return HttpResponse(template.render())
+    context = {
+            'project_list': project_list,
+    }
+    return HttpResponse(template.render(context))
